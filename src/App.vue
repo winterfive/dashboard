cfbab13dfd00e28350574138d4b1ffcd353209dd
Lee Gainer <piealacode@gmail.com>
2019-07-19 15:45:57 -0400

    Fixed prettier tabs

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,20 @@
 <template>
   <v-app>
-    <v-card class="mx-auto mt-5 pa-2">
-      <v-card-title class="pb-0">
-        <h1>Login</h1>
-      </v-card-title>
-      <v-card-text>
-        <v-form>
-          <v-text-field 
-            label="Username"
-            prepend-icon="account_circle" />
-          <v-text-field
-            :type="showPassword ? 'text' : 'password'"
-            label="Password"
-            prepend-icon="lock"
-            :append-icon="showPassword ? 'visibility_on' : 'visibility_off'" 
-            @click:append="showPassword = !showPassword"/>
-        </v-form>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn color="success">Register</v-btn>
-        <v-btn color="info">Login</v-btn>
-      </v-card-actions>
-    </v-card>
+    <Form></Form>
   </v-app>
 </template>
 
 <script>
 
+import Form from "./components/Form.vue";
+
 export default {
   name: 'App',
   data () {
     return {
-      showPassword: false
     }
   }
 }
 </script>
 
-<style>
 
-.v-card {
-  width: 400px;
-}
-</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card class="mx-auto mt-5 pa-2">
+    <v-card-title class="pb-0">
+      <h1>Login</h1>
+    </v-card-title>
+    <v-card-text>
+      <v-form>
+        <v-text-field 
+          label="Username"
+          prepend-icon="account_circle" />
+        <v-text-field
+          :type="showPassword ? 'text' : 'password'"
+          label="Password"
+          prepend-icon="lock"
+          :append-icon="showPassword ? 'visibility_on' : 'visibility_off'" 
+          @click:append="showPassword = !showPassword"/>
+        </v-form>
+      </v-card-text>
+    <v-card-actions>
+      <v-btn color="success">Register</v-btn>
+      <v-btn color="info">Login</v-btn>
+    </v-card-actions>
+  </v-card>    
+</template>
+
+<script>
+export default {
+  data() {
+    showPassword: false
+  }
+    
+}
+</script>
+
+<style>
+
+.v-card {
+width: 400px;
+}
+</style>
+
+
